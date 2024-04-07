1c95751adc901cd5a6a25c89470934ddf79cf4e3
Jessica Duft <130928718+JessicaDuft@users.noreply.github.com>
2024-04-07 13:35:27 -0500

    Add files via upload

---
+++ Enhanced_Code/app_server/routes/rooms.js
@@ -0,0 +1,12 @@
+
+
+
+
+const express = require('express');
+const router = express.Router();
+const controller = require ('../controllers/rooms');
+
+router.get('/', controller.roomList);
+
+
+module.exports = router;
